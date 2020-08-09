a623a466f5370ab78775e5da8189a134b7633464
Vasiliy Pupkin <vpupkin@company.com>
2020-08-09 19:24:42 +0300

    верстка

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*********************  плавный скролл    ***************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
+
+/************    burger      *************/
+let user_icon = document.querySelector('.burger');
+user_icon.addEventListener("click", function (e) {
+  let user_menu = document.querySelector('.main-menu');
+  user_menu.classList.toggle('show');
+  user_icon.classList.toggle('active');
+});
+
+document.documentElement.addEventListener("click", function (e) {
+  if (!e.target.closest('.burger')) {
+    let user_menu = document.querySelector('.main-menu');
+    user_menu.classList.remove('show');
+    user_icon.classList.remove('active');
+  }
+});
